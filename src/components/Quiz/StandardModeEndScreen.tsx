32d40d5435bfa885a6396219328ccd2464a8abd6
Mario Torres <mariotorresdev@gmail.com>
2025-10-04 22:35:07 -0500

    fix: formatting scroll

--- src/components/Quiz/StandardModeEndScreen.tsx
+++ src/components/Quiz/StandardModeEndScreen.tsx
@@ -26,7 +26,7 @@ export function StandardModeEndScreen({
   setQuizResult,
   setSelectedAnswer
 }: IProps) {
-  return <div className="text-center m-[35%_10%] lg:m-[15%_10%]">
+  return <div className="text-center m-[15%_10%] lg:m-[5%_10%]">
     <p className="text-2xl md:text-4xl font-bold mb-4 h-32">Quiz Complete! You answered {Object.values(userResponses).filter(response => response.selected === response.correct).length} out of {standardQuizLength} questions correctly.</p>
     <button className="btn btn-primary text-lg p-4" onClick={() => {
       setQuizzesCompleted([]);
@@ -40,7 +40,7 @@ export function StandardModeEndScreen({
     <div className={"mt-8 text-left p-8 " + globalBackgroundStyle}>
       <details>
         <summary className="text-xl font-bold mb-4 cursor-pointer">Review Your Answers</summary>
-        <div className="mt-4">
+        <div className="mt-4 max-h-80 overflow-y-scroll">
           {
             userResponses.map((response) => {
               const quiz = currentQuizSet.find(q => q.id === response.quizId)!;
