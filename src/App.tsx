7929b0a4fcc7aed0548d895a777f074a749b2197
Mobyus1 <mobyus1@hotmail.com>
2025-10-08 13:28:12 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,9 @@ import './App.css';
 import { AudioContextProvider } from './util/context';
 
 function App() {
-  const clickSound = React.useMemo(() => new Audio({
-    volume: 0.1,
-    src: ['/assets/sfx/click.mp3']
-    }), []);
-  const confirmSound = React.useMemo(() => new Audio({
-    volume: 0.1,
-    src: ['/assets/sfx/confirm.mp3']
-    }), []);
-  const transitionSound = React.useMemo(() => new Audio({
-    volume: 0.1,
-    src: ['/assets/sfx/transition.mp3']
-    }), []);
+  const clickSound = React.useMemo(() => new Audio('/assets/sfx/click.mp3, []);
+  const confirmSound = React.useMemo(() => new Audio('/assets/sfx/confirm.mp3', []);
+  const transitionSound = React.useMemo(() => new Audio('/assets/sfx/transition.mp3', []);
 
   const sfx = (type: SfxType) => {
     let sound: HTMLAudioElement;
