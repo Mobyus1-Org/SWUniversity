592c54aee9d68dfba9d0f8e4c10247331944e91c
Mobyus1 <mobyus1@hotmail.com>
2025-10-08 21:42:33 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ function App() {
   const clickSound = React.useMemo(() => new Audio('/assets/sfx/click.mp3'), []);
   clickSound.volume = 0.05;
   const confirmSound = React.useMemo(() => new Audio('/assets/sfx/confirm.mp3'), []);
-  confirmSound.volume = 0.10;
+  confirmSound.volume = 0.05;
   const transitionSound = React.useMemo(() => new Audio('/assets/sfx/transition.mp3'), []);
-  transitionSound.volume = 0.20;
+  transitionSound.volume = 0.05;
 
   const sfx = (type: SfxType) => {
     let sound: HTMLAudioElement;
