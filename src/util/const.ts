746be885cb2527caebf7947986244f761cfdd441
Mobyus1 <mobyus1@hotmail.com>
2025-10-12 11:15:49 -0400

    Update const.ts

--- src/util/const.ts
+++ src/util/const.ts
@@ -2,5 +2,5 @@ export const globalBackgroundStyle = "bg-[rgba(23,35,87,0.8)]";
 export type SWUniversityApp = "quiz" | "dykswu";
 export type AppModes = "" | "marathon" | "endless" | "standard" | "padawan" | "knight" | "master";
 export type SfxType = "click" | "confirm" | "transition";
-export type DYKSWUChoice = "name" | "card-type" | "arena" | "subtitle" | "cost" | "aspects" | "power" | "hp" | "traits" | "trigger-condition" | "ability-text" | "rarity" | "art" | "something-else" | "no-change";
-export const DYKSWUChoices: DYKSWUChoice[] = ["name", "card-type", "arena", "subtitle", "cost", "aspects", "power", "hp", "traits", "trigger-condition", "ability-text", "rarity", "art", "something-else", "no-change"];
+export type DYKSWUChoice = "name" | "card-type" | "arena" | "subtitle" | "cost" | "aspects" | "power" | "hp" | "traits" | "trigger-condition" | "play-restriction" | "ability-text" | "rarity" | "art" | "something-else" | "no-change";
+export const DYKSWUChoices: DYKSWUChoice[] = ["name", "card-type", "arena", "subtitle", "cost", "aspects", "power", "hp", "traits", "trigger-condition", "play-restriction", "ability-text", "rarity", "art", "something-else", "no-change"];
