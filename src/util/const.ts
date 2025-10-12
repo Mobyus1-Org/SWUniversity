26248265348ed25542860469f8bf745dd6ccad37
Mobyus1 <mobyus1@hotmail.com>
2025-10-12 03:09:25 -0400

    Update const.ts

--- src/util/const.ts
+++ src/util/const.ts
@@ -2,5 +2,5 @@ export const globalBackgroundStyle = "bg-[rgba(23,35,87,0.8)]";
 export type SWUniversityApp = "quiz" | "dykswu";
 export type AppModes = "" | "marathon" | "endless" | "standard" | "padawan" | "knight" | "master";
 export type SfxType = "click" | "confirm" | "transition";
-export type DYKSWUChoice = "name" | "cardtype" | "arena" | "subtitle" | "cost" | "aspects" | "power" | "hp" | "traits" | "trigger-condition" | "ability-text" | "rarity" | "art";
-export const DYKSWUChoices: DYKSWUChoice[] = ["name", "cardtype", "arena", "subtitle", "cost", "aspects", "power", "hp", "traits", "trigger-condition", "ability-text", "rarity", "art"];
+export type DYKSWUChoice = "name" | "card-type" | "arena" | "subtitle" | "cost" | "aspects" | "power" | "hp" | "traits" | "trigger-condition" | "ability-text" | "rarity" | "art";
+export const DYKSWUChoices: DYKSWUChoice[] = ["name", "card-type", "arena", "subtitle", "cost", "aspects", "power", "hp", "traits", "trigger-condition", "ability-text", "rarity", "art"];
