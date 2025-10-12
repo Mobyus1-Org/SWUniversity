95fc3bc1c8c89527e6f62200d9d4018324e66fcf
Mobyus1 <mobyus1@hotmail.com>
2025-10-12 01:41:56 -0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,8 @@ function HomePage() {
   return <div className="grid md:grid-cols-3 gap-4 md:gap-8">
     <div className={`p-4 border md:col-span-2 ${globalBackgroundStyle}`}>
       <h1 className="text-3xl uwd:text-4xl 4k:text-5xl uwd:mb-2 4k:mb-4 font-bold">Welcome to SWUniversity!</h1>
-      <p className="text-lg uwd:text-2xl 4k:text-4xl 4k:mb-4">This is a place where STAR WARS: Unlimited players can test their knowledge of the game they love to play!</p>
+      <p className="text-lg uwd:text-2xl 4k:text-4xl 4k:mb-4"><br />This is a place where STAR WARS: Unlimited players can test their knowledge of the game they love to play!<br /></p>
       <p className="text-lg uwd:text-2xl 4k:text-4xl 4k:mb-4">
-        <br />
         Please be patient as we continue to develop the site!
         <br />
         <br />
