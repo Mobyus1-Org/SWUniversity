a168d54cc048688401993750ade1136e6a15f20a
Mario Torres <mariotorresdev@gmail.com>
2025-10-16 23:01:44 -0500

    fix: added SWUniversity homepage logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { globalBackgroundStyle } from "../util/const";
 function HomePage() {
   return <div className="grid md:grid-cols-3 gap-4 md:gap-8">
     <div className={`p-4 border md:col-span-2 ${globalBackgroundStyle}`}>
-      <h1 className="text-3xl uwd:text-4xl 4k:text-5xl uwd:mb-2 4k:mb-4 font-bold">Welcome to SWUniversity!</h1>
+      <h1 className="text-3xl uwd:text-4xl 4k:text-5xl uwd:mb-2 4k:mb-4 font-bold">Welcome to the <img src="/assets/HomepageLogo.png" alt="SWUniversity Banner" className="my-4 uwd:my-8"/></h1>
       <p className="text-lg uwd:text-2xl 4k:text-4xl 4k:mb-4"><br />This is a place where STAR WARS: Unlimited players can test their knowledge of the game they love to play!</p>
       <p className="text-lg uwd:text-2xl 4k:text-4xl 4k:mb-4"><br />
         Please be patient as we continue to develop the site! We're planning on having multiple game modes which will be accessible from the menu at the top of your screen! Keep checking back for more updates!
