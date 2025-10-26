b92c9640de35975e082eb74ab4071b0eeb8875eb
Mobyus1 <mobyus1@hotmail.com>
2025-10-26 04:40:55 -0400

    feat: Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,8 +3,11 @@ import { globalBackgroundStyle } from "../util/style-const";
 function AboutPage() {
   return <div className={`p-20 border h-[85vh] ${globalBackgroundStyle}
       text-center text-3xl uwd:text-5xl 4k:text-7xl font-bold flex flex-col gap-12`}>
-    <h1 className="text-7xl">About Us</h1>
-    <p>Learn more about our mission, vision, and values. Would you happen to have a minute to talk about the Midichlorians?</p>
+    <h1 className="text-7xl">About SWUniversity</h1>
+    <p>SWUniversity is a passion project operated by a small group of players interested in helping others learn STAR WARS: Unlimited, and have fun doing so!</p>
+    <p>Project Lead: Mobyus1</p>
+    <p>Programming: ninin</p>
+    <p>Quizmaster: Chanter</p>
   </div>;
 }
 
