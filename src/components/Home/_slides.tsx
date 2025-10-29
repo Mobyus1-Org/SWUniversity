3669f66c134fa5ad375331a7b7a314200dfd486b
Mobyus1 <mobyus1@hotmail.com>
2025-10-29 07:07:47 -0400

    feat: Update _slides.tsx

--- src/components/Home/_slides.tsx
+++ src/components/Home/_slides.tsx
@@ -17,14 +17,14 @@ export const slides: ICarouselData[] = [
     type: "paragraph",
     title: "Iron Man Challenge Is Here!",
     src: "/assets/rivals-fall-splash.png",
-    description: "We're excited to announce the launch of the Iron Man Challenge! Test your skills and see how many questions you can complete in a row without any mistakes. This new mode is designed to push your knowledge and endurance to the limit.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. Etiam euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque.",
+    description: "We're excited to announce the launch of the Iron Man Challenge! Test your skills and see how many questions you can complete in a row without any mistakes. This new mode is designed to push your knowledge and endurance to the limit.",
   },
   {
     type: "banner-text",
     alt: "image of rule of the week, from rule book",
     title: "Rule of the Week: The Hand Zone",
-    src: "https://gcdnb.pbrd.co/images/XNKjp7GUJN0N.png?o=1",
-    description: "The hand is a special zone where cards are kept before they are played. Players can only play cards from their hand during their turn.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. Etiam euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. Etiam euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque.",
+    src: "/assets/banners/hand.png",
+    description: "The hand is a special zone where cards are kept before they are played. Players can only play cards from their hand during their turn unless a card specifies otherwise. Cards in a player's hand are considered \"hidden information,\" meaning a player is not required to divulge any information about what they may or may not be holding. If an opponent asks you a question about what a particular card says or does, don't be afraid to call a judge over to provide an answer as to not unintentionally hint at what may be in your hand!",
   },
   {
     type: "embed",
