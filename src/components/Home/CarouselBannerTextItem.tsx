72b7e73ec42c3e1e6cb88eb63fe3e73d27964c82
Mario Torres <mariotorresdev@gmail.com>
2025-10-30 00:55:59 -0500

    fix: parsed new lines for BannerText component

--- src/components/Home/CarouselBannerTextItem.tsx
+++ src/components/Home/CarouselBannerTextItem.tsx
@@ -2,13 +2,19 @@ import type { ICarouselItemProps } from "./_slides";
 
 export function CarouselBannerTextItem({ data }: ICarouselItemProps) {
   const { src, alt, description } = data;
+  const formattedDescription = description?.split("\n").map((line, index) => (
+    <span key={index}>
+      {line}
+      <br />
+    </span>
+  ));
   return (
     <div className="flex flex-col items-center justify-start w-full text-center overflow-hidden">
       <div className="w-full aspect-[12/1] mb-8 rounded shadow-lg overflow-hidden flex items-center justify-center">
         <img src={src} alt={alt || 'news image'} className="w-full h-full object-cover" />
       </div>
       <div className="overflow-y-auto w-full flex-1 px-2">
-        <p className="text-lg uwd:text-2xl 4k:text-4xl mb-4">{description}</p>
+        <p className="text-lg uwd:text-2xl 4k:text-4xl mb-4">{formattedDescription}</p>
       </div>
     </div>
   );
