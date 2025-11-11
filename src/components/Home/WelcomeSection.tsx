c915262ff9b41ea803bf37884820ad0add2f5cbb
Mobyus1 <mobyus1@hotmail.com>
2025-11-11 17:14:50 -0500

    fix: Update WelcomeSection.tsx

--- src/components/Home/WelcomeSection.tsx
+++ src/components/Home/WelcomeSection.tsx
@@ -3,7 +3,10 @@ export function WelcomeSection() {
     <h1 className="text-3xl uwd:text-4xl 4k:text-5xl uwd:mb-2 4k:mb-4 font-bold">Welcome to the <img src="/assets/HomepageLogo.png" alt="SWUniversity Banner" className="my-4 uwd:my-8"/></h1>
     <p className="text-md uwd:text-2xl 4k:text-4xl 4k:mb-4">This is a place where STAR WARS: Unlimited players can test their knowledge of the game they love to play!</p>
     <p className="text-md uwd:text-2xl 4k:text-4xl 4k:mb-4"><br />
-      Please be patient as we continue to develop the site! We're planning on having multiple game modes which will be accessible from the menu at the top of your screen! Keep checking back for more updates!
+      The site is now live! Use the menu in the top-left of the page to access our different game modes! 
+      <br />
+      <br />
+      In addition to constantly updating our databases, we also have many exciting features planned for the future, including both quality of life features, as well as entirely new game modes! Keep checking back to see what's been added!
       <br />
       <br />
       Thank you and have fun!
