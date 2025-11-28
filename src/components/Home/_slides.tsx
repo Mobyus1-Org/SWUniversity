eeb8a4b166329d954bbc42a52ac020a66f5df739
Mobyus1 <mobyus1@hotmail.com>
2025-11-28 14:00:46 -0500

    fix: Update _slides.tsx

--- src/components/Home/_slides.tsx
+++ src/components/Home/_slides.tsx
@@ -26,13 +26,6 @@ export const slides: ICarouselData[] = [
     description: "We're excited to announce the launch of the Iron Man Challenge! Test your skills and see how many questions you can complete in a row without making any mistakes. This new mode is designed to push your knowledge and endurance to the limit.\n\nHaving fun on SWUniversity.net? Stay tuned for more unique ways to test yourself coming in the future!",
   },
   {
-    type: "banner-text",
-    alt: "image of rule of the week, from rule book",
-    title: "Rule of the Week: The Hand Zone",
-    src: "/assets/banners/hand.png",
-    description: "The hand is a special zone where cards are kept before they are played. Players can only play cards from their hand during their turn unless a card specifies otherwise. Cards in a player's hand are considered \"hidden information,\" meaning a player is not required to divulge any information about what they may or may not be holding.\n\nIf an opponent asks you a question about what a particular card says or does, don't be afraid to call a judge over to provide an answer as to not unintentionally hint at what may be in your hand!",
-  },
-  {
     type: "embed",
     title: "Does FFG Know SWU?",
     src: "https://www.youtube.com/embed/hWPWmIUUXcI",
