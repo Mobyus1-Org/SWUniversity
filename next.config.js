06712de83e7c1c357bd8ded34b5e70126dae4c10
Mario Torres <mariotorresdev@gmail.com>
2026-05-22 01:17:15 -0500

    fix: nextjs bundle

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['mongoose', 'sharp'],
+  outputFileTracingExcludes: {
+    '*': ['./public/assets/**/*'],
+  },
 };
 
 export default nextConfig;
