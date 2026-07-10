8a0bf768cb7312f37b8a14c0cb260061ab37e8bf
Mario Torres <mariotorresdev@gmail.com>
2026-07-10 01:29:56 -0500

    fix: UI leader list

--- src/containers/PuzzlesPage.tsx
+++ src/containers/PuzzlesPage.tsx
@@ -89,6 +89,8 @@ const LEADERS_WITH_ACTION_ABILITY = new Set([
   "SHD_016", "SHD_017",
   //Twilight of the Republic
   "TWI_005", "TWI_012",
+  //Legends of the Underworld
+  "LAW_008",
 ]);
 
 // Non-leader units with an Action ability. Maps cardId → short label for the modal button.
